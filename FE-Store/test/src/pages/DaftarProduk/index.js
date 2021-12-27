c1a954b198cdd971049cace3433c22c4bb5b6172
akbaralfarizii <akbargates21@gmail.com>
2021-12-28 03:10:51 +0700

    update DaftarProduk,SettingProduk

--- FE-Store/test/src/pages/DaftarProduk/index.js
+++ FE-Store/test/src/pages/DaftarProduk/index.js
@@ -1,122 +1,127 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity, StyleSheet, Text, ScrollView, Image } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, Text, ScrollView, Image, FlatList } from 'react-native';
 
 const DaftarProduk = ({navigation}) => {
-  return(
-    <ScrollView style={styles.background}>
-      <Text>KOSONG</Text>
-    </ScrollView>
-  );
+
+   const [dataProduk, setDataProduk] = useState([]);
+
+
+    const DATA = [
+      {
+        produk_id: '1',
+        stok: '20',
+        foto_Produk: 'https://www.polygonbikes.com/wp-content/uploads/2021/03/Thumbnail-Blog-Polygon-Bikes-1024x683.jpg',
+        nama_produk: 'sepeda polygon',
+        harga_produk: '2500000',
+        rating_produk: '4.2',
+      },
+      {
+        produk_id: '2',
+        stok: '12',
+        foto_Produk: 'https://reactnative.dev/img/tiny_logo.png',
+        nama_produk: 'sepeda pacific',
+        harga_produk: '20000',
+        rating_produk: '4.2',
+      },
+      {
+        produk_id: '3',
+        stok: '13',
+        foto_Produk: 'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/26/1441917955.png',
+        nama_produk: 'sepeda united',
+        harga_produk: '30000',
+        rating_produk: '4.2',
+      },
+      
+    ];
+
+    return (
+      <View style={styles.container}>
+            <FlatList
+            numColumns={2}
+            keyExtractor={( item ) => item.produk_id}
+            data={DATA}
+            renderItem={({ item }) => (
+                <View style={styles.cardProduk}>
+                    <View style={styles.stok}>
+                        <Text style={styles.textStok}>Stok {item.stok}</Text>
+                    </View>
+                    <View>
+                        <Image source={{uri:item.foto_produk}} style={styles.gambar}/>
+                    </View>
+                    <View>
+                        <Text style={styles.nama}>{item.nama_produk}</Text>
+                    </View>
+                    <View style={styles.keterangan}>
+                        <Text style={styles.textHarga}>Rp.{item.harga_produk}</Text>
+                        <Image source={require('../../assets/rating.png')} style={styles.ratingLogo}/>
+                        <Text style={styles.textRating}>{item.rating_produk}</Text>
+                    </View>
+                    
+                </View>
+            )}
+            />
+      </View>
+    )
 };
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    backgroundColor: 'white',
-    
-  },
   container: {
-    margin: 20,
-    
-  },
-  containerSeparator: {
-    margin: 10,
-
-  },
-
-  topTitle: {
-    fontSize: 16,
-    color: 'black',
-  },
-  Title: {
-    fontSize: 16,
-    color: 'black',
-    marginVertical: 10
-  },
-    TitleHarga: {
-    fontSize: 16,
-    color: 'black',
-    marginTop: 10,
-
-  },
-  input: {
-    justifyContent: 'flex-start',
-    fontSize: 14,
-    borderColor: '#CECECE',
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10
-  },
-  inputHarga: {
-    justifyContent: 'flex-start',
-    fontSize: 14,
-    borderColor: '#CECECE',
-    borderRadius: 5,
-    borderBottomWidth: 2
-  },
-  inputVariant: {
-    justifyContent: 'flex-start',
-    fontSize: 14,
-    borderColor: '#CECECE',
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginHorizontal: 5,
-    marginVertical:10
-  },
-  inputStock: {
-    justifyContent: 'flex-start',
-    fontSize: 14,
-    borderColor: '#CECECE',
-    borderBottomWidth: 2,
-    borderRadius: 5,
-    padding: 10,
-    marginHorizontal: 5,
-    marginVertical:10
-  },
-  picker: {
-    justifyContent: 'flex-start',
-    fontSize: 14,
-    borderColor: '#CECECE',
-    borderWidth: 1,
-    borderRadius: 5,
-  },
-  besidePicture: {
-    justifyContent: "center",
-    marginStart: 10,
-    marginEnd: 50
-  },
-  picture: {
-    width: 60,
-    height: 60,
-    borderRadius: 10,
-    marginVertical: 10
-    
-  },
-  border: {
-    width: '100%',
-    height: 9,
-    backgroundColor: '#F4F4F4',
-    marginTop: 20
-  },
-
-  button: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    borderRadius: 10,
-    backgroundColor: "#0C8EFF",
-    marginTop: 15,
-    //textAlign: "center",
-  },
-  
-  produk: {
-    height: '20%',
-    backgroundColor: "red",
-    borderWidth: 5,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-   
-  },
+        backgroundColor: 'white',
+        height: '100%',
+        width: '100%',
+        alignItems: 'center'
+    },
+    cardProduk: {
+        top: '3%',
+        height: '90%',
+        width: '49%',
+        backgroundColor: 'white',
+        marginHorizontal: 2,
+        borderRadius: 10,
+        marginBottom: 20,
+        padding: 15,
+        flexDirection: 'column',
+        borderWidth: 0.5,
+        borderColor: 'lightgrey'
+    },
+    stok: {
+        top: -10
+    },
+    keterangan: {
+        position: 'absolute',
+        left: '10%',
+        top: '108%',
+        width: '100%',
+    },
+    ratingLogo: {
+        position: 'absolute',
+        left: '67%',
+        width: 18,
+        height: 18
+    },
+    gambar: {
+        left: '10%',
+        width: 80,
+        height: 80,
+    },
+    nama: {
+        fontSize: 8,
+        color: 'black'
+    },
+    textStok: {
+        color: 'black',
+        fontSize: 10,
+    },
+    textHarga: {
+        position: 'absolute',
+        color: 'steelblue',
+        fontSize: 12,
+    },
+    textRating: {
+        color: 'gray',
+        fontSize: 12,
+        left: '80%',
+    }
 });
 
 export default DaftarProduk;
--- FE-Store/test/src/pages/SettingProduk/index.js
+++ FE-Store/test/src/pages/SettingProduk/index.js
@@ -1,13 +1,145 @@
 import React, { useState } from 'react';
-import { View, FlatList, TouchableOpacity, StyleSheet, Text, ScrollView, Image, TextInput } from 'react-native';
+import { View, FlatList, TouchableOpacity, StyleSheet, Text, ScrollView, Image, Switch } from 'react-native';
+import Setting from '../../assets/Pen.svg';
 
 const SettingProduk = ({navigation}) => {
-  //
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const DATA = [
+      {
+        produk_id: '1',
+        stok: '20',
+        foto_Produk: 'https://www.polygonbikes.com/wp-content/uploads/2021/03/Thumbnail-Blog-Polygon-Bikes-1024x683.jpg',
+        nama_produk: 'sepeda pacific',
+        harga_produk: '2500000',
+        rating_produk: '4.2',
+      },
+      {
+        produk_id: '2',
+        stok: '12',
+        foto_Produk: 'https://reactnative.dev/img/tiny_logo.png',
+        nama_produk: 'sepeda pacific',
+        harga_produk: '20000',
+        rating_produk: '4.2',
+      },
+      {
+        produk_id: '3',
+        stok: '13',
+        foto_Produk: 'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/26/1441917955.png',
+        nama_produk: 'sepeda united',
+        harga_produk: '30000',
+        rating_produk: '4.2',
+      },
+      
+    ];
+
   return (
-    <View>
-    
+    <View style={styles.background}>
+      <View flexDirection= 'row' justifyContent='space-between' style={styles.Container}>
+        <Text style={{fontSize:12, left: 10, fontWeight:'bold'}}>Info Produk</Text>
+        <Text style={{fontSize:12, left: 20, fontWeight:'bold'}}>Stock</Text>
+        <Text style={{fontSize:12, left: -30, fontWeight:'bold'}}>Status</Text>
+      </View>
+      <View style={{
+        width: '100%',
+        height: 1,
+        backgroundColor: '#E4E4E4'
+      }}/>
+      <View style={styles.Container}>
+        <FlatList
+            
+            keyExtractor={( item ) => item.produk_id}
+            data={DATA}
+            renderItem={({ item }) => (
+              <View flexDirection= 'row' alignItems='center' style={{marginVertical:5}}>
+                <Text>{item.produk_id}.</Text>
+                <Image source={require('../../assets/rating.png')} style={{width: 50, height: 50}}/>
+                <View justifyContent='center' >
+                  <Text style={{fontSize:12}}>{item.nama_produk}</Text>
+                  <Text style={{fontSize:12}}>Rp. {item.harga_produk}</Text>
+                </View>
+                <Text style={{fontSize:12, left:60}}>{item.stok}</Text>
+                <Switch
+                  trackColor={{ false: "#767577", true: "#81b0ff" }}
+                  thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                  ios_backgroundColor="#3e3e3e"
+                  onValueChange={toggleSwitch}
+                  value={isEnabled}
+                  style={{left:130}}
+                />
+                <TouchableOpacity>
+                  <Setting style={{left:130}}/>
+                </TouchableOpacity>
+              </View>
+            )}
+            />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  background: {
+        backgroundColor: 'white',
+        height: '100%',
+        width: '100%',
+    },
+    Container: {
+        marginHorizontal: 20,
+        marginVertical: 10,
+    },
+    cardProduk: {
+        top: '3%',
+        height: '90%',
+        width: '49%',
+        backgroundColor: 'white',
+        marginHorizontal: 2,
+        borderRadius: 10,
+        marginBottom: 20,
+        padding: 15,
+        flexDirection: 'column',
+        borderWidth: 0.5,
+        borderColor: 'lightgrey'
+    },
+    stok: {
+        top: -10
+    },
+    keterangan: {
+        position: 'absolute',
+        left: '10%',
+        top: '108%',
+        width: '100%',
+    },
+    ratingLogo: {
+        position: 'absolute',
+        left: '67%',
+        width: 18,
+        height: 18
+    },
+    gambar: {
+        left: '10%',
+        width: 80,
+        height: 80,
+    },
+    nama: {
+        fontSize: 8,
+        color: 'black'
+    },
+    textStok: {
+        color: 'black',
+        fontSize: 10,
+    },
+    textHarga: {
+        position: 'absolute',
+        color: 'steelblue',
+        fontSize: 12,
+    },
+    textRating: {
+        color: 'gray',
+        fontSize: 12,
+        left: '80%',
+    }
+});
 
 export default SettingProduk;
